3c15b08f5320cef9326dd05cebeb7540c894f392
Santiago <60631810+endemoniau@users.noreply.github.com>
2022-06-17 20:40:39 -0300

    Delete Android/app/build directory

--- Android/app/build/generated/ap_generated_sources/debug/out/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new com.elendemo.btar2812.DataBinderMapperImpl());
-  }
-}
--- Android/app/build/generated/data_binding_trigger/debug/com/elendemo/btar2812/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.elendemo.btar2812;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
